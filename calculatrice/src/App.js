b8315f5ef05047c7065cfd417a9c781ced976a42
FI$$ <FISSSSSS@proton.me>
2023-11-01 10:15:41 +0100

    feat : addition and clear inputs

--- calculatrice/src/App.js
+++ calculatrice/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [value1, setValue1] = useState('');
+  const [value2, setValue2] = useState('');
+  const [result, setResult] = useState('');
+
+
+  function clearInputs(){
+    setValue1("");
+    setValue2("");
+    setResult("");
+  }
+
+
+  function Addition() {
+    const num1 = parseInt(value1, 10);
+    const num2 = parseInt(value2, 10);
+    const sum = num1 + num2;
+    setResult(sum);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator-container">
+      <h1 className="calculator-title">Simple calculator</h1>
+      <div className="calculator-inputs">
+        <input type="text" id="value1" name="value1" placeholder="NUMBER e.g. 1337" value={value1} onChange={(e) => setValue1(e.target.value)}/>
+        <input type="text" id="value2" name="value2" placeholder="NUMBER e.g. 2108" value={value2} onChange={(e) => setValue2(e.target.value)}/>
+      </div>
+      <div className="calculator-buttons">
+        <button type="button" onClick={Addition}>+ Add</button>
+        <button type="button">- Substract</button>
+        <button type="button">/ Divide</button>
+        <button type="button">x Multiply</button>
+      </div>
+      <div className="calculator-result-reset">
+      <input type="text" id="result" name="result" placeholder="Result" value={result}/>
+      <button className="button-reset" type="button" onClick={clearInputs}>Clear</button>
+      </div>
     </div>
   );
 }
